962446006f09f5fd181b7ab7f158be23831e7d9e
Stanislav Shershnev <shershnev.stas@gmail.com>
2019-07-26 19:16:04 +0200

    version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='drf_orjson',
-    version='0.2',
+    version='0.2.1',
     description='DRF ORJSON Renderer',
     license='MIT',
     author='Stanislav Shershnev',
